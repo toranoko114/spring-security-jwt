132b629d6d7497db4919287b5a39bf1bb94af7b4
toranoko114 <toranoko114.business@gmail.com>
2023-04-15 17:27:41 +0900

    SpringBoot VersionUp

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS user;
+set mode MySQL;
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS users (
     user_id bigint not null primary key auto_increment,
     username varchar(50) not null unique,
     password varchar(100),
     role varchar(100) default 'ROLE_USER'
-)
+);
